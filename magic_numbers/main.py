7110f61ded7b3a4bc8bcd6f78c7b86bd19e0beb2
Botond Budahazy <botondbudahazi2007@gmail.com>
2026-04-17 15:17:28 +0200

    implement power from ^

--- magic_numbers/main.py
+++ magic_numbers/main.py
@@ -1,6 +1,16 @@
 from pathlib import Path
 
 
+
+
+def check_for_power(num: str):
+    if num.__contains__("^"):
+        a = int(num.split("^")[0])
+        b = int(num.split("^")[1])
+        return a**b
+    else:
+        return int(num)
+
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
     print(data, end="")
