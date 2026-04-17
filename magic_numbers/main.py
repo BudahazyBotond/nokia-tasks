670564eefb123aa813a77c8b699733a881c09cfd
BudahazyBotond <botondbudahazi2007@gmail.com>
2026-04-17 18:41:25 +0200

    refactor convert_to_magic_nums to check magic numbers and implement odd length conversion logic

--- magic_numbers/main.py
+++ magic_numbers/main.py
@@ -7,12 +7,43 @@ def convert_to_magic_nums(nums:list):
             if nums[i][j] != "9":
                 allNines = False
         if allNines:
-            print(conver_to_magic_num_if_all_nines(nums[i]))
+            check_if_magic_num(conver_to_magic_num_if_all_nines(nums[i]))
         elif len(nums[i]) % 2 == 0:
-            print(convert_if_even_length(nums[i]))
+            check_if_magic_num(convert_if_even_length(nums[i]))
         elif len(nums[i]) % 2 == 1:
-            print("odd")
+            check_if_magic_num(convert_if_odd_length(nums[i]))
     
+def convert_if_odd_length(num:str):
+    length = len(num)
+    chars = list(num)
+    initial_chars = chars
+    if length == 1 and chars[0] < "9":
+        return str(int(num)+1)
+    else:
+        middle = chars[int(length/2+0.5-1)]
+        del chars[int(length/2+0.5-1)]
+        length = int(length - 1)
+        for i in range(int(length/2)-1,-1,-1):
+            if chars[i] == chars[length-1-i]:
+                continue
+            elif chars[i] > chars[length-1-i]:
+                chars[length-1-i] = chars[i]
+                if not i == 0:
+                    for j in range(length-i,length):
+                        chars[j] = "0"
+            else: 
+                chars[i] = str(int(chars[i])+1)
+                chars[length-1-i] = chars[i]
+                if not i == 0:
+                    for j in range(length-i,length):
+                        chars[j] = "0"
+            #print(list(map(str,chars)))
+        chars.insert(int(length/2), middle)
+        if initial_chars == chars:
+            chars[int(length/2+0.5)] = str(int(chars[int(length/2+0.5)])+1)
+        num = ''.join(chars)
+        return num
+
 def brute_force_check(nums:list):
     
     for i in range(len(nums)):
@@ -48,9 +79,9 @@ def conver_to_magic_num_if_all_nines(num:str):
 
 def check_if_magic_num(num:str):
     if num[::-1] == num:
-        return True
+        print(num)
     else:
-        return False
+        print("hiba: " + num)
 
 def delete_empty_or_letter_occurrances(nums:list):
     for i in range(len(nums)):
