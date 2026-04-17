cd6b0872b031e3c914ff8c714bb0008aa75f0188
Botond Budahazy <botondbudahazi2007@gmail.com>
2026-04-17 15:37:26 +0200

    implement solution if all nines

--- magic_numbers/main.py
+++ magic_numbers/main.py
@@ -1,6 +1,12 @@
 from pathlib import Path
 
 
+
+
+
+def conver_to_magic_num_if_all_nines(num:str):
+    return f"1{(len(num)-1)*'0'}1"
+
 def check_if_magic_num(num:str):
     if num[::-1] == num:
         return True
@@ -16,6 +22,7 @@ def check_for_power(num: str):
         return int(num)
 
 def main():
+    print(conver_to_magic_num_if_all_nines("9999"))
     data = Path("input.txt").read_text(encoding="utf-8")
     print(data, end="")
 
