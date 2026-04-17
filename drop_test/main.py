fc9e83ccf058433320bbb95d23634c80705ce3bf
BudahazyBotond <botondbudahazi2007@gmail.com>
2026-04-17 22:39:44 +0200

    Implemented cheat sheet (memo) and optimized min_drops

--- drop_test/main.py
+++ drop_test/main.py
@@ -4,19 +4,27 @@ def do_the_drops(data:list):
     for line in data:
         N = int(line[0])
         H = int(line[1])
-        print(min_drops(N, H))
+        print(bike_drop(N, H))
 
-def min_drops(N:int, H:int):
-    if H <= 1: return H
-    if N == 1: return H
-    
-    min = H
-    for i in range(1, H+1):
-        drops = int(max(min_drops(N-1, i-1), min_drops(N, H-i)))
-        if drops < min:
-            min = drops
-    return min + 1
+def min_drops(N:int, H:int, memo:list):
+    if memo[N][H] != -1:
+        return memo[N][H]
+    if H == 1 or H == 0:
+        return H
+    if N == 1:
+        return H
+    res = H
+    for i in range(1, H + 1):
+        cur = max(min_drops(N - 1, i - 1, memo), \
+                    min_drops(N, H - i, memo))
+        if cur < res:
+            res = cur
+    memo[N][H] = res + 1
+    return memo[N][H]
 
+def bike_drop(N, H):
+    memo = [[-1 for _ in range(H + 1)] for _ in range(N + 1)]
+    return min_drops(N, H, memo)
 
 def line_parse(line:list):
     for i in range(len(line)):
