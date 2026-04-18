da2a6299f50979889f15892825ab0c9d0f166eee
BudahazyBotond <botondbudahazi2007@gmail.com>
2026-04-18 10:22:59 +0200

    Read files

--- ipconfig_parser/main.py
+++ ipconfig_parser/main.py
@@ -2,9 +2,15 @@ from pathlib import Path
 
 
 def main():
-    for path in sorted(Path(".").glob("*.log")):
-        print(path.name)
+    paths = []
+    for path in sorted(Path(".").glob("*.txt")):
+        paths.append(path.name)
+    a_path = Path(paths[0]).read_text(encoding="utf-8").splitlines()
+    b_path = Path(paths[1]).read_text(encoding="utf-8").splitlines()    
+    print(a_path)
+    print(b_path)
 
 
 if __name__ == "__main__":
     main()
+    
