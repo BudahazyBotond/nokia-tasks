d25b4fa266c3c03bd37521d107830fde51e7330b
BudahazyBotond <botondbudahazi2007@gmail.com>
2026-04-18 12:29:59 +0200

    refactor get_adapters function for clarity and added last adapter detection

--- ipconfig_parser/main.py
+++ ipconfig_parser/main.py
@@ -1,24 +1,32 @@
 from pathlib import Path
 
-def get_adapters(path:list):
+def get_adapters(original_path:list):
+    path = original_path.copy()
     adapters = []
     in_same_adapter = True
-    how_many_adapters = int(get_adapters_count(path))
-    j=0
-    for i in range(how_many_adapters):
-        
-        while in_same_adapter:
-            adapter = []
-            for j in range(len(path)):
-                line = path[j]
-                if line.split(" ")[:3] == ["Ethernet", "adapter", "Ethernet"]:
-                    in_same_adapter = False
-                    break
-                adapter.append(line)
-            adapters.append(adapter)
+    is_last_adapter = False
+    i = 0
+    while(is_last_adapter == False):
+        if path[i].split(" ")[:3] == ["Ethernet", "adapter", "Ethernet"]:
+            print("Found adapter: " + path[i].split(" ")[3:][0].split(":")[0])
+            while in_same_adapter:
+                adapter = []
+                for j in range(len(path)):
+                    line = path[j]
+                    if line.split(" ")[:3] == ["Ethernet", "adapter", "Ethernet"]:
+                        in_same_adapter = False
+                        path = path[j:]
+                        if line.split(" ")[3:][0].split(":")[0] == get_last_adapter(original_path):
+                            print("Found last adapter: " + line.split(" ")[3:][0].split(":")[0])
+                            is_last_adapter = True
+                        break
+                    adapter.append(line)
+                adapters.append(adapter)
+        in_same_adapter = True
+        i+=1
     return adapters
 
-def get_adapters_count(path:list):
+def get_last_adapter(path:list):
     path = path[::-1]
     for line in path:
         if line.split(" ")[:3] == ["Ethernet", "adapter", "Ethernet"]:
@@ -31,12 +39,19 @@ def dump_list(path:list):
     for line in path:
         print(line)
 
+def del_empty_lines(path:list):
+    new_path = []
+    for line in path:
+        if not len(line) < 0 and not line == len(line)*" ":
+            new_path.append(line)
+    return new_path
+
 def main():
     paths = []
     for path in sorted(Path(".").glob("*.txt")):
         paths.append(path.name)
-    a_path = Path(paths[0]).read_text(encoding="utf-8").splitlines()
-    b_path = Path(paths[1]).read_text(encoding="utf-8").splitlines()    
+    a_path = del_empty_lines(Path(paths[0]).read_text(encoding="utf-8").splitlines())
+    b_path = del_empty_lines(Path(paths[1]).read_text(encoding="utf-8").splitlines())
     dump_list(get_adapters(a_path))
     #dump_list(a_path)
     #dump_list(b_path)
